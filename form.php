89a3621ab6628c25195d3a1240773f64133f7b92
Quba2403 <67878712+Quba2403@users.noreply.github.com>
2020-07-20 16:32:14 +0200

    Add files via upload

--- form.php
+++ form.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+    require('login.php');
+    
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,15 +15,58 @@
     <title>Document</title>
 </head>
 <body>
+ <style>
 
-<h1>Nowy rekord</h1>
-<form method = "post" action = "login.php">
-Imie: <input name = "name" type = "text" id = "name"><br>
-Nazwisko: <input name = "surname" type = "text" id = "surname"><br>
-Hasło: <input name = "pass" type = "text" id = "pass"><br>
-<input name = "add" type = "submit" id = "add" value = "Zaloguj się">
+body {
+  background: url(tlo.png);
+  background-size: cover;
+}
+
+.logo {
+    width: 400px;
+    height: 200px;
+}
+
+.form {
+  position: absolute;
+  top: 40%;
+  left: 44%;
+  color: #3cbabe;
+}
+
+.forma {
+  position: absolute;
+  top: 30%;
+  left: 44%;
+  color: #3cbabe;
+}
+
+#add {
+  text-decoration: none;
+  text-align: center;
+  color: #3cbabe;
+  border: 2px solid #3cbabe;
+  border-radius: 10%;
+  width: 150px;
+  line-height: 40px;
+  display: inline-block;
+  font-family: Arial;
+  font-weight: 600;
+  font-size: 20px;
+  background-color: gray;
+}
+</style> 
+
+
+<div ><a href="https://www.artcom.pl/"></a> <img class="logo" src="logo.png" alt="logo">
+<h1 class="forma">Zaloguj się!!!</h1>
+<form class="form" method = "post" action = "<?php $_PHP_SELF ?>">
+<h2>Imie:<h2> <input name = "name" type = "text" id = "name"><br>
+<h2>Nazwisko:<h2> <input name = "surname" type = "text" id = "surname"><br>
+<h2>Hasło:<h2> <input name = "pass" type = "password" id = "pass"><br> <br>
+<input  name = "add" type = "submit" id = "add" value = "Zaloguj się" >
 </form>
 </body>
 </html>
 
-<!-- <?php $_PHP_SELF ?> -->
+
--- login.php
+++ login.php
@@ -1,50 +1,27 @@
 <?php
-//gotowy do otwarcia sesji
-session_start();
 //połączenie z bazą danych, w osobnym pliku (connect.php) lub przepisujemy dane
 require('connect.php');
-if (isset($_POST['username']) and isset($_POST['password'])){
-$username = $_POST['username'];
-$password = $_POST['password'];
-//próbuję wybrać z bazy rekord z danych formularza
-$query = "SELECT * FROM `user` WHERE username='$username' and password='$password'";
-$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-//liczę rekordy
-$count = mysqli_num_rows($result);
-if ($count == 1){
-//jeśli znalazłem jeden dopasowany rekord, to znaczy że dane są poprawne
-//ustawiam sesję "username" z nazwą użytkownika
-$_SESSION['username'] = $username;
-header('Location: secret.php');
-}
-else {
-//co robię jeśli podano złe hasło
-$_POST['message'] = 'Zue hasuo xD';
-header('Location: form.php');
-}
-}
-?>
+if (isset($_POST['name']) and isset($_POST['pass'])){
+    $username = $_POST['name'];
+    $password = $_POST['pass'];
+    //próbuję wybrać z bazy rekord z danych formularza
+    $query = "SELECT * FROM `user` WHERE name='$username' and pass='$password'";
+    $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+    //liczę rekordy
+    $count = mysqli_num_rows($result);
+    if ($count == 1){
+        //jeśli znalazłem jeden dopasowany rekord, to znaczy że dane są poprawne
+        //ustawiam sesję "username" z nazwą użytkownika
+        $_SESSION['name'] = $username;
 
-
-<!-- secret.php -->
-<?php
-if (isset($_SESSION['username'])){
-//jeśli mam sesję logowania to robię menu
-$username = $_SESSION['username'];
-echo "Witaj ".$username;
-echo "<a href='logout.php'>Wyloguj</a>";
+        // echo $_SESSION['name'];
+        header('Location: index.php');
+        // echo "ok";
+    }
+    else {
+        //co robię jeśli podano złe hasło
+        $_POST['message'] = 'Zue hasuo xD'; 
+        header('Location: index.php');
+    }
 }
-else {
-//jeśli próbuję się tu dostać, a nie jestem zalogowany - wypad
-header('Location: index.php');
-}
-?>
-
-
-
-<!-- logout.php -->
-<?php
-session_start();
-session_destroy();
-header('Location: index.php');
 ?>
